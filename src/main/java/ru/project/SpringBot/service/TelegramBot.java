3b98ef63c4db4ef7c3cc6506e35a1bcd5d434d61
Artem Slukin <rebuss96@mail.ru>
2022-12-10 21:27:35 +0300

    Add list commands

--- src/main/java/ru/project/SpringBot/service/TelegramBot.java
+++ src/main/java/ru/project/SpringBot/service/TelegramBot.java
@@ -3,10 +3,16 @@ package ru.project.SpringBot.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.project.SpringBot.config.BotConfig;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -16,6 +22,19 @@ public class TelegramBot extends TelegramLongPollingBot /*Webhook*/ {
 
     public TelegramBot(BotConfig config) {
         this.config = config;
+        List<BotCommand> listCommands = new ArrayList<>();
+        listCommands.add(new BotCommand("/start", "welcome message"));
+        listCommands.add(new BotCommand("/funnystore", "твой любимый анекдот"));
+        listCommands.add(new BotCommand("/mydata", "get your data store"));
+        listCommands.add(new BotCommand("/deletedata", "delete my data"));
+        listCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listCommands.add(new BotCommand("/settings", "set your preferences"));
+
+        try {
+            this.execute(new SetMyCommands(listCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bots commands list " + e.getMessage());
+        }
     }
 
     @Override
@@ -38,7 +57,12 @@ public class TelegramBot extends TelegramLongPollingBot /*Webhook*/ {
                 case "/start":
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                 break;
-                    default: sendMessage(chatId, "Sorry, не знаю такой команды братанчик");
+                case "/funnystore":
+                    sendMessage(chatId, "Пупа и Лупа красили яйцо на пасху," +
+                            " но у Лупы сломалась кисточка." +
+                            " И Пупе пришлось красить за Лупу.");
+                    break;
+                    default: sendMessage(chatId, "Извини, не знаю такой команды.");
             }
         }
     }
@@ -46,7 +70,7 @@ public class TelegramBot extends TelegramLongPollingBot /*Webhook*/ {
     private void startCommandReceived(Long chatId, String name) {
 
 
-        String answer = "Hi, " + name + ", nice to meet you";
+        String answer = "Привет, " + name + ", приятно познакомиться";
         log.info("Replied to user " + name);
 
         sendMessage(chatId, answer);
@@ -56,6 +80,7 @@ public class TelegramBot extends TelegramLongPollingBot /*Webhook*/ {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSend);
+        log.info(message.getText());
 
         try {
             execute(message);
